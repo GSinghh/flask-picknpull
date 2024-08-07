4abdc0b1425a1e9f6f369a570adaeed8d08837c8
GSinghh <50472056+GSinghh@users.noreply.github.com>
2024-08-06 22:44:30 -0700

    modified function to work with config file

--- app/__init__.py
+++ app/__init__.py
@@ -4,11 +4,6 @@ from .extensions import db
 
 def create_app():
     app = Flask(__name__)
-    app.config["SQLALCHEMY_DATABASE_URI"] = (
-        "postgresql://user:password@localhost:5432/postgres"
-    )
+    app.config.from_object("app.config.Config")
     db.init_app(app)
-
-
-if __name__ == "__main__":
-    create_app()
+    return app
