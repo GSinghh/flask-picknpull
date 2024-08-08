170f9419ba8dbdb2e5211e9f47936460a6acfdda
GSinghh <50472056+GSinghh@users.noreply.github.com>
2024-08-07 23:53:48 -0700

    added database migration functionality

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,13 @@
 from flask import Flask
+from flask_migrate import Migrate
 from .extensions import db
+from .routes.user_routes import main
 
 
 def create_app():
     app = Flask(__name__)
     app.config.from_object("app.config.Config")
     db.init_app(app)
+    app.register_blueprint(main)
+    migate = Migrate(app, db)
     return app
