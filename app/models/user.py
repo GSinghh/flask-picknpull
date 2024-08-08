72426a1779604c44699c9debf536273a58dd29ae
GSinghh <50472056+GSinghh@users.noreply.github.com>
2024-08-07 23:55:31 -0700

    Fixed typo __table__name -> __tablename__

--- app/models/user.py
+++ app/models/user.py
@@ -2,10 +2,10 @@ from ..extensions import db
 
 
 class User(db.Model):
-    __table__name = "Users"
+    __tablename__ = "Users"
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(100), nullable=False, unique=True)
-    phone_number = db.Column(db.String(32), nullable=False)
+    number = db.Column(db.String(32), nullable=False, unique=True)
 
     def __repr__(self) -> str:
-        return f"id: {self.id}, email: {self.email}, phone_number: {self.phone_number}"
+        return f"id: {self.id}, email: {self.email}, phone_number: {self.number}"
