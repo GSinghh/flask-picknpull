9feb44297ba39ecfe91944c462a20a973b894232
GSinghh <50472056+GSinghh@users.noreply.github.com>
2024-08-14 01:13:05 -0700

    Created Navbar component

---
+++ frontend/src/components/Navbar.tsx
@@ -0,0 +1,10 @@
+const Navbar = () => {
+    return (
+        <div className="navbar">
+            <div className="navbar-logo"></div>
+            <div className="navbar-links"></div>
+        </div>
+    );
+};
+
+export default Navbar;
