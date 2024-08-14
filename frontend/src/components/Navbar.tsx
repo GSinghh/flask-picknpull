33d4f7ed8bc2f32a86bef4c9e8015f220f9e5e25
GSinghh <50472056+GSinghh@users.noreply.github.com>
2024-08-14 15:28:27 -0700

    Created Navbar and styles file

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,8 +1,18 @@
+import "../styles/Navbar.css";
+
 const Navbar = () => {
     return (
         <div className="navbar">
-            <div className="navbar-logo"></div>
-            <div className="navbar-links"></div>
+            <div className="navbar-container">
+                <div className="navbar-links-left">
+                    <a href="/">New</a>
+                    <a href="/">Upload</a>
+                    <a href="/">Share</a>
+                </div>
+                <div className="navbar-links-right">
+                    <div className="v-divider"></div>
+                </div>
+            </div>
         </div>
     );
 };
