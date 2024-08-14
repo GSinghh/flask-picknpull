2336799730974e8a95d04e9a32eed30e44b93ee4
GSinghh <50472056+GSinghh@users.noreply.github.com>
2024-08-13 21:32:57 -0700

    Implemented post, get, delete routes for API

--- app/routes/user_routes.py
+++ app/routes/user_routes.py
@@ -1,25 +1,51 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, request
+from flask_restful import Api, Resource
 from ..models.user import User
 from ..extensions import db
 
 main = Blueprint("main", __name__)
+api = Api(main)
 
 
-@main.route("/signup/<email>/<number>")
-def signup_user(email, number):
-    Users = User(email=email, number=number)
-    db.session.add(Users)
-    db.session.commit()
+class Users(Resource):
+    def post(self):
+        try:
+            data = request.get_json()
+            email = data.get("email")
+            number = data.get("number")
+            
+            user = User(email=email, number=number)
+            db.session.add(user)
+            db.session.commit()
+            return {"Message": f"User with email: {email} and number: {number} added succesfully"}
+        except Exception as e:
+            return {"Error": f"Exception: {e}"}
 
-    return "User added successfully"
+    def get(self):
+        try:
+            all_users = User.query.all()
+            if all_users:
+                return jsonify([user.serialize() for user in all_users])        
+            else:
+                return {"Error": "Users not Found"}
+        except Exception as e:
+            return {"Error": f"Exception: {e}"}
 
+    def delete(self):
+        try:
+            data = request.get_json()
+            email = data.get("email")
+            number = data.get("number")
+            found_user = User.query.filter_by(email=email, number=number).first()
+            
+            if found_user:
+                db.session.delete(found_user)
+                db.session.commit()
+                return {"Message": f"User with email: {email} and number: {number} deleted"}
+            else:
+                return {"Error": "User Not Found"}
+        except Exception as e:
+            return {"Error": f"{e}"}
 
-@main.route("/records", methods=["GET"])
-def get_all_users():
-    all_users = User.query.all()
-    return jsonify([user.serialize() for user in all_users])
-
-
-@main.route("/delete/<email>/<number>")
-def delete_user(email, number):
-    pass
+        
+api.add_resource(Users, "/Users")
