fd2640b50943e302841ad81a3bf96aa195195121
GSinghh <50472056+GSinghh@users.noreply.github.com>
2024-08-15 00:27:02 -0700

    Implemented React Router for sidebar

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter as Router } from "react-router-dom";
+import "./index.css";
+import Navbar from "./components/Navbar";
+import Sidebar from "./components/Sidebar";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+createRoot(document.getElementById("root")!).render(
+    <StrictMode>
+        <Router>
+            <div className="container">
+                <Sidebar />
+                <Navbar />
+            </div>
+        </Router>
+    </StrictMode>
+);
