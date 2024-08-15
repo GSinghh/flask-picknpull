d7781f1611313cba8e1c0c65236f01e9bf0d4f6d
GSinghh <50472056+GSinghh@users.noreply.github.com>
2024-08-15 00:28:48 -0700

    Implemented Navigation on sidebar

--- frontend/src/components/Sidebar.tsx
+++ frontend/src/components/Sidebar.tsx
@@ -1,15 +1,23 @@
 import "../styles/Sidebar.css";
-
+import { Link } from "react-router-dom";
 const Sidebar = () => {
     return (
         <div className="sidebar">
-            <div className="sidebar-logo">Admin Dashboard</div>
-            <div className="divider"></div>
+            <div className="sidebar-logo">Dashboard</div>
+            <div className="h-divider"></div>
             <ul>
-                <li>Add Users</li>
-                <li>Delete User</li>
-                <li>Update User</li>
-                <li>List All Users</li>
+                <li>
+                    <Link to="/">Add User</Link>
+                </li>
+                <li>
+                    <Link to="/">Delete User</Link>
+                </li>
+                <li>
+                    <Link to="/">Update User</Link>
+                </li>
+                <li>
+                    <Link to="/">List All Users</Link>
+                </li>
             </ul>
         </div>
     );
