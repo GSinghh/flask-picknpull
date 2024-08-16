4102a40e52e857d5343bdf7a2a35f81b99357d61
GSinghh <50472056+GSinghh@users.noreply.github.com>
2024-08-16 16:29:10 -0700

    Created Horizontal Panel Component for Grid Layout

---
+++ frontend/src/components/HorizontalPanel.tsx
@@ -0,0 +1,25 @@
+import "../styles/HorizontalPanel.css";
+
+const HorizontalPanel = () => {
+    return (
+        <div className="horizontal-panel">
+            <div className="h-panel-title">Horizontal Panel</div>
+            <div className="h-panel-body">
+                <p>
+                    Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                    Error corrupti, saepe quia, modi quas exercitationem
+                    distinctio nulla ipsum, nisi sequi quod est soluta! Eveniet,
+                    sapiente?
+                </p>
+            </div>
+        </div>
+
+        // <div class="project-card">
+        //             <div class="project-image" style="background-image: url(images/project-1.jpg);"></div>
+        //             <h4>Title Below Image</h4>
+        //             <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Error corrupti, saepe quia, modi quas exercitationem distinctio nulla ipsum, nisi sequi quod est soluta! Eveniet, sapiente?</p>
+        //             <div class="sidebuttons"></div>
+        //           </div>
+    );
+};
+export default HorizontalPanel;
