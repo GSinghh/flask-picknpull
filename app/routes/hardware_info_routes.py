1de3a091802fbd5a8e77790502a3cc17e1be8067
GSinghh <50472056+GSinghh@users.noreply.github.com>
2024-08-18 23:57:57 -0700

    Created API Endpoints for hardware analytics

---
+++ app/routes/hardware_info_routes.py
@@ -0,0 +1,47 @@
+from flask import Blueprint
+from flask_restful import Api, Resource
+import psutil
+
+hardware = Blueprint("hardware", __name__)
+api = Api(hardware)
+
+class CPUInformation(Resource):
+    def get(self):
+        try:
+            cpu_usage = psutil.cpu_percent(interval=1)
+            cpu_freq = psutil.cpu_freq()
+            load_avg = psutil.getloadavg()
+            return {"CPU Usage": cpu_usage, "CPU Freq": cpu_freq, "Avg Load": load_avg}
+        except Exception as e:
+            return {"Error": f"Exception: {e}"}
+        
+class MemoryInformation(Resource):
+    def get(self):
+        try:
+            disk_usage = psutil.disk_usage('/')
+            total_usage = f"{disk_usage.total / (1024 ** 3):.2f}"
+            used_space = f"{disk_usage.used / (1024 ** 3):.2f}"
+            free_space = f"{disk_usage.free / (1024 ** 3):.2f}"
+            usage_percenage = f"{disk_usage.percent}%"
+            
+            
+            memory_info = psutil.virtual_memory()
+            total_memory = f"{memory_info.total / (1024 ** 3):.2f} GB"
+            available_memory = f"{memory_info.available / (1024 ** 3):.2f} GB"
+            used_memory = f"{memory_info.used / (1024 ** 3):.2f} GB"
+            memory_usage = f"{memory_info.percent}%"
+                        
+            return {"Used Disk Space": used_space,
+                    "Free Disk Space":free_space,
+                    "Total Disk Space":total_usage,
+                    "Disk Usage Percentage":usage_percenage,
+                    "Available Memory": available_memory,
+                    "Total Memory":total_memory,
+                    "Used Memory": used_memory,
+                    "Memory Usage Percetange": memory_usage}
+            
+        except Exception as e:
+            return {"Error": f"Exception: {e}"}
+            
+api.add_resource(CPUInformation, "/cpu-info")
+api.add_resource(MemoryInformation, "/mem-info")
