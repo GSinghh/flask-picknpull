16e5039f7acd1cc19375b523344d34efda081daa
GSinghh <50472056+GSinghh@users.noreply.github.com>
2024-08-20 13:16:16 -0700

    Added CORS to backend

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask_cors import CORS
 from flask_migrate import Migrate
 from .extensions import db
 from .routes.user_routes import main
@@ -7,6 +8,7 @@ from .routes.hardware_info_routes import hardware
 
 def create_app():
     app = Flask(__name__)
+    CORS(app)
     app.config.from_object("app.config.Config")
     db.init_app(app)
     app.register_blueprint(main)
