8f6081c247093e92c24a8374b6d6894ae51b54c1
GSinghh <50472056+GSinghh@users.noreply.github.com>
2024-08-28 23:55:28 -0700

    Created model for each vehicle

--- app/models/user.py
+++ app/models/user.py
@@ -12,3 +12,15 @@ class User(db.Model):
 
     def serialize(self):
         return {"id": self.id, "email": self.email, "number": self.number}
+
+class Vehicle(db.Model):
+    __tablename__ = "vehicles"
+    id = db.Column(db.Integer, primary_key=True)
+    start_year = db.Column(db.String, nullable=False, unique=False)
+    end_year = db.Column(db.String, nullable=False, unique=False)
+    postal_code = db.Column(db.String, nullable=False, unique=False)
+    distance = db.Column(db.String, nullable=False, unique=False)
+    make = db.Column(db.String, nullable=False, unique=False)
+    model = db.Column(db.String, nullable=False, unique=False)
+    make_id = db.Column(db.String, nullable=False, unique=False)
+    model_id = db.Column(db.String, nullable=False, unique=False)
