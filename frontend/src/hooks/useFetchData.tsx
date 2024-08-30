c96897a8447a67f1c5d795f7418fd9dfb2e71d9c
GSinghh <50472056+GSinghh@users.noreply.github.com>
2024-08-30 14:46:52 -0700

    Created typescript generic for hook data

--- frontend/src/hooks/useFetchData.tsx
+++ frontend/src/hooks/useFetchData.tsx
@@ -1,28 +1,14 @@
 import { useState, useEffect } from "react";
 
-type CPUData = {
-    cpuUsage: number;
-    cpuFreq: number[];
-    loadAvg: number[];
-    responseCode: number;
-};
-type MemData = {
-    [key: string]: string | number;
-    responseCode: number;
-};
-type Error = {
-    message: string | number | null;
-    responseCode: number;
-};
-type fetchDataState = {
-    data: CPUData | MemData | Error | null;
+export function useFetchData<T>(url: string): {
+    data: T | null;
     loading: boolean;
     error: string | null;
-};
-export function useFetchData(url: string): fetchDataState {
-    const [data, setData] = useState<CPUData | MemData | Error | null>(null);
+} {
+    const [data, setData] = useState<T | null>(null);
     const [loading, setLoading] = useState<boolean>(true);
     const [error, setError] = useState<string | null>(null);
+
     useEffect(() => {
         const fetchData = async () => {
             try {
@@ -33,7 +19,7 @@ export function useFetchData(url: string): fetchDataState {
                 const data = await response.json();
                 setData(data);
             } catch (error: any) {
-                setError(error);
+                setError(error.message || "An Error Occurred");
             } finally {
                 setLoading(false);
             }
