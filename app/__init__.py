e44440a36b38e1ef3210949b77701404bcd28991
GSinghh <50472056+GSinghh@users.noreply.github.com>
2024-09-05 23:59:18 -0700

    added redis and celery

--- app/__init__.py
+++ app/__init__.py
@@ -4,12 +4,14 @@ from flask_migrate import Migrate
 from .extensions import db
 from .routes.user_routes import main
 from .routes.hardware_info_routes import hardware
+from celery import Celery
 
 
 def create_app():
     app = Flask(__name__)
     CORS(app)
     app.config.from_object("app.config.Config")
+    celery = Celery(app.name, broker=app.config["CELERY_BROKER_URL"], backend=app.config["CELERY_BACKEND_URL"])
     db.init_app(app)
     app.register_blueprint(main)
     app.register_blueprint(hardware)
