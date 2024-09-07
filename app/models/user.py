476d1330468b2aa7f14c2055e41ca3a5102d1563
GSinghh <50472056+GSinghh@users.noreply.github.com>
2024-09-06 18:43:24 -0700

    Added relationships between user and vehicles

--- app/models/user.py
+++ app/models/user.py
@@ -6,21 +6,10 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(100), nullable=False, unique=True)
     number = db.Column(db.String(32), nullable=False, unique=True)
-
+    vehicles = db.relationship('Vehicle', back_populates='owner', cascade='all, delete-orphan')
+    
     def __repr__(self) -> str:
         return f"id: {self.id}, email: {self.email}, phone_number: {self.number}"
 
     def serialize(self):
         return {"id": self.id, "email": self.email, "number": self.number}
-
-class Vehicle(db.Model):
-    __tablename__ = "vehicles"
-    id = db.Column(db.Integer, primary_key=True)
-    start_year = db.Column(db.String, nullable=False, unique=False)
-    end_year = db.Column(db.String, nullable=False, unique=False)
-    postal_code = db.Column(db.String, nullable=False, unique=False)
-    distance = db.Column(db.String, nullable=False, unique=False)
-    make = db.Column(db.String, nullable=False, unique=False)
-    model = db.Column(db.String, nullable=False, unique=False)
-    make_id = db.Column(db.String, nullable=False, unique=False)
-    model_id = db.Column(db.String, nullable=False, unique=False)
