ccb922e48d403c7d79029f3e6c741715bcd70c5e
GSinghh <50472056+GSinghh@users.noreply.github.com>
2024-09-10 15:22:01 -0700

    created one to many relation between user and vehicles

--- app/models/user.py
+++ app/models/user.py
@@ -1,12 +1,13 @@
 from ..extensions import db
 
-
 class User(db.Model):
     __tablename__ = "users"
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(100), nullable=False, unique=True)
     number = db.Column(db.String(32), nullable=False, unique=True)
-    vehicles = db.relationship('Vehicle', back_populates='owner', cascade='all, delete-orphan')
+    carrier = db.Column(db.String(32), nullable=True, unique=False)
+    
+    vehicles = db.relationship('Vehicle', back_populates='user', cascade='all, delete-orphan')
     
     def __repr__(self) -> str:
         return f"id: {self.id}, email: {self.email}, phone_number: {self.number}"
