e5e1c3c46f66d58265a65613d5cf98c04501ed40
GSinghh <50472056+GSinghh@users.noreply.github.com>
2024-09-10 18:19:15 -0700

    Implemented results table to store webscraped results

--- app/models/vehicle.py
+++ app/models/vehicle.py
@@ -1,15 +1,17 @@
 from ..extensions import db
+from sqlalchemy import ForeignKey
 
 class Vehicle(db.Model):
     __tablename__ = "vehicles"
     id = db.Column(db.Integer, primary_key=True)
-    start_year = db.Column(db.String, nullable=False, unique=False)
-    end_year = db.Column(db.String, nullable=False, unique=False)
-    postal_code = db.Column(db.String, nullable=False, unique=False)
-    distance = db.Column(db.String, nullable=False, unique=False)
-    make = db.Column(db.String, nullable=False, unique=False)
-    model = db.Column(db.String, nullable=False, unique=False)
+    start_year = db.Column(db.String(16), nullable=True, unique=False)
+    end_year = db.Column(db.String(16), nullable=True, unique=False)
+    postal_code = db.Column(db.String(16), nullable=False, unique=False)
+    distance = db.Column(db.String(16), nullable=False, unique=False)
+    make = db.Column(db.String(16), nullable=False, unique=False)
+    model = db.Column(db.String(16), nullable=False, unique=False)
     
-    owner = db.relationship('User', back_populates='vehicles')
+    user_id = db.Column(db.Integer, ForeignKey('users.id'), unique=True)
+    user = db.relationship('User', back_populates='vehicles')
     
-    results = db.relationship('Result', back_populates='vehicles', cascade='')
+    results = db.relationship('Result', back_populates='vehicle', cascade='delete-orphan', uselist=False)
