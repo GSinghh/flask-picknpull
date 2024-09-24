ba164516502a2899476fabe7766ff650351466ed
GSinghh <50472056+GSinghh@users.noreply.github.com>
2024-09-23 23:53:22 -0700

    created getIDByUser function

--- app/models/user.py
+++ app/models/user.py
@@ -7,10 +7,13 @@ class User(db.Model):
     number = db.Column(db.String(32), nullable=False, unique=True)
     carrier = db.Column(db.String(32), nullable=True, unique=False)
     
-    vehicles = db.relationship('Vehicle', back_populates='user', cascade='all, delete-orphan')
+    vehicles = db.relationship('Vehicle', back_populates='user', cascade='all, delete, delete-orphan')
     
     def __repr__(self) -> str:
         return f"id: {self.id}, email: {self.email}, phone_number: {self.number}"
 
     def serialize(self):
         return {"id": self.id, "email": self.email, "number": self.number}
+    
+    def getIDByUser(self, number):
+        return self.id if number == self.number else None
