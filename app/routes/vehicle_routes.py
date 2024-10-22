c86e357d106f8a95d7640c8188cea5d8fe6de888
GSinghh <50472056+GSinghh@users.noreply.github.com>
2024-10-21 23:41:22 -0700

    created GET and POST endpoints

--- app/routes/vehicle_routes.py
+++ app/routes/vehicle_routes.py
@@ -1,19 +1,47 @@
-from flask import Blueprint 
+from flask import Blueprint, request, jsonify
 from flask_restful import Api, Resource
 from ..models.vehicle import Vehicle
+from ..models.user import User
 from ..extensions import db
 
-vehicles = Blueprint("vehicles",  __name__)
-api = Api(vehicles)
+
+vehicles_bp = Blueprint("vehicles",  __name__)
+api = Api(vehicles_bp)
 
 class Vehicles(Resource):
     def get(self):
-        pass
-    
+        try:
+            data = request.get_json()
+            number = data.get("number")
+            user_id = User.get_id_by_number(number)
+            vehicles = Vehicle.getVehicles(user_id)
+            return {"Vehicles": f"Vehicles: {vehicles}"}
+        except Exception as e:
+            return {"error": f"An error occured: {e}"}
+            
     def post(self):
-        pass
+        try:
+            data = request.get_json()
+            
+            number = data.get("number")
+            user_id = User.get_id_by_number(number)
+            
+            start_year = data.get("start_year")
+            end_year = data.get("end_year")
+            postal_code = data.get("postal_code")
+            distance = data.get("distance")
+            make = data.get("make")
+            model = data.get("model")
+            
+            vehicle = Vehicle(start_year=start_year, end_year=end_year, postal_code=postal_code, distance=distance, make=make, model=model, user_id=user_id)
+            db.session.add(vehicle)
+            db.session.commit()
+            
+            return {"message": f"{start_year + " " +  end_year} {make} {model} Added"}
+        except Exception as e:
+            return {"error": f"Error Occured: {e}"}
     
     def delete(self):
         pass
     
-api.add_resource(vehicles, "/vehicles")
+api.add_resource(Vehicles, "/vehicles")
