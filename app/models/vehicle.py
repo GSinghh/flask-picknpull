a54a2407ed463e67b2b3d1416f8d445f1982e549
GSinghh <50472056+GSinghh@users.noreply.github.com>
2024-10-23 17:30:47 -0700

    added function for grabbing by id, validating years, displaying the object data, and serializing

--- app/models/vehicle.py
+++ app/models/vehicle.py
@@ -11,7 +11,34 @@ class Vehicle(db.Model):
     make = db.Column(db.String(16), nullable=False, unique=False)
     model = db.Column(db.String(16), nullable=False, unique=False)
     
-    user_id = db.Column(db.Integer, ForeignKey('users.id'), unique=True)
+    user_id = db.Column(db.Integer, ForeignKey('users.id'))
     user = db.relationship('User', back_populates='vehicles')
     
-    results = db.relationship('Result', back_populates='vehicle', cascade='delete-orphan', uselist=False)
+    results = db.relationship('Result', back_populates='vehicle', cascade='delete, delete-orphan', uselist=False)
+    
+    @classmethod
+    def get_vehicles_by_id(cls, user_id_param: int):
+        vehicles = cls.query.filter_by(user_id = user_id_param).all()
+        return vehicles
+    
+    @staticmethod
+    def validate_years(start_year, end_year):
+        if start_year != "" and end_year == "":
+            return start_year
+        elif start_year == "" and end_year != "":
+            return end_year
+        else:
+            return f"{start_year}-{end_year}"
+    
+    def __repr__(self) -> str:
+        return f"{self.validate_years(self.start_year, self.end_year)} {self.make} {self.model}"
+    
+    def serialize(self):
+        return {"id": self.id,
+                "start_year": self.start_year,
+                "end_year": self.end_year,
+                "postal_code": self.postal_code,
+                "distance": self.distance,
+                "make": self.make, 
+                "model": self.model}
+    
