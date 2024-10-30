292b57abd3a60e37277a1cf768b257346b4d38ad
GSinghh <50472056+GSinghh@users.noreply.github.com>
2024-10-29 17:40:16 -0700

    deleted both files

--- app/models/models.py
+++
@@ -1,7 +0,0 @@
-from ..extensions import db
-
-class Model(db.Model):
-    __tablename__ = "models"
-    id = db.Column(db.Integer, primary_key=True)
-    model_name = db.Column(db.String(16), nullable=False, unique=True)
-    model_code = db.Column(db.String(8), nullable=False, unique=True)
