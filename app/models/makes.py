3ee09c9e33b0992fbd3a0e6bb940a0f6fa82fd86
GSinghh <50472056+GSinghh@users.noreply.github.com>
2024-10-29 17:36:57 -0700

    Increased string size from 16 -> 32 for make and model schema

--- app/models/makes.py
+++ app/models/makes.py
@@ -3,5 +3,5 @@ from ..extensions import db
 class Make(db.Model):
     __tablename__ = "makes"
     id = db.Column(db.Integer, primary_key=True)
-    make_name = db.Column(db.String(16), nullable=True, unique=True)
+    make_name = db.Column(db.String(32), nullable=True, unique=True)
     make_code = db.Column(db.String(8), nullable=False, unique=True)
