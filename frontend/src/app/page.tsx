88792a4dc8a5bd88d61fefeca270223d5637674a
GSinghh <50472056+GSinghh@users.noreply.github.com>
2024-11-19 21:31:46 -0800

    added in github and lightmode icon

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,4 +1,6 @@
 import { AppSidebar } from "@/components/app-sidebar";
+import { BrainCog, Sun } from "lucide-react";
+import { Button } from "@/components/ui/button";
 import {
     Breadcrumb,
     BreadcrumbItem,
@@ -7,6 +9,8 @@ import {
     BreadcrumbPage,
     BreadcrumbSeparator,
 } from "@/components/ui/breadcrumb";
+
+import { Github } from "lucide-react";
 
 import { Separator } from "@/components/ui/separator";
 
@@ -17,6 +21,7 @@ import {
 } from "@/components/ui/sidebar";
 
 import { CPUPieChart } from "@/components/CPUPieChart";
+import { MemoryUsage } from "@/components/MemoryUsage";
 
 export default function Page() {
     return (
@@ -46,14 +51,21 @@ export default function Page() {
                             </BreadcrumbList>
                         </Breadcrumb>
                     </div>
-                    <div className="flex gap-2 px-5">Testing</div>
+                    <div className="flex gap-2 px-5">
+                        <Button variant={"outline"}>
+                            <Github />
+                        </Button>
+                        <Button variant={"outline"}>
+                            <Sun />
+                        </Button>
+                    </div>
                 </header>
                 <div className="flex flex-1 flex-col gap-4 p-4">
                     <div className="grid auto-rows-min gap-4 md:grid-cols-3">
                         <div className="col-span-2">
                             <CPUPieChart />
                         </div>
-                        <CPUPieChart />
+                        <MemoryUsage />
                     </div>
                     <div className="grid auto-rows-min gap-4 md:grid-cols-3">
                         <CPUPieChart />
