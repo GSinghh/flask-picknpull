5d3468d5be86b185775e1468c74d75afe438b297
GSinghh <50472056+GSinghh@users.noreply.github.com>
2024-11-29 16:39:37 -0800

    added websocket functionality

--- run.py
+++ run.py
@@ -1,7 +1,7 @@
-from app import create_app
+from app import create_app, socketio
 
-
-app = create_app()
+app = create_app(debug=True)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    # app.run(debug=True)
+    socketio.run(app)
